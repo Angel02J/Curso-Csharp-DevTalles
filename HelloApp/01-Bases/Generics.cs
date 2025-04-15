8b9f5d552f92b729248c7f9ccddcca28a634ac9b
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2025-04-15 08:51:42 -0600

    Generics

--- HelloApp/01-Bases/Generics.cs
+++ HelloApp/01-Bases/Generics.cs
@@ -8,6 +8,11 @@ partial class Program
         System.Console.WriteLine($"Tamaño del arreglo númerico {GetInArrayLength(numbers)}");
         System.Console.WriteLine($"Tamaño del arreglo nombres {GetStringArrayLength(names)}");
         System.Console.WriteLine($"Tamaño del arreglo nombres {GetArrayLength(names)}");
+
+        Box<int> numberBox = new Box<int>{ Content = 50};
+        Box<string> stringBox = new Box<string>{ Content = "Ahora soy un string"};
+        numberBox.Show();
+        stringBox.Show();
     }
 
     static int GetInArrayLength(int[] array)
@@ -24,5 +29,15 @@ partial class Program
     static int GetArrayLength<T>(T[] array)
     {
         return array.Length;
+    }
+
+    class Box<T>
+    {
+        public T? Content { get; set; }
+
+        public void Show()
+        {
+            System.Console.WriteLine($"Contenido: {Content}");
+        }
     }
 }
